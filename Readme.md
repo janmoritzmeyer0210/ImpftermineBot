d05724b3489928d75312d6932aa0fa153a329824
Jan Moritz <janmoritzmeyer@yahoo.com>
2021-05-13 18:45:59 +0200

    Support for multiple selenium servers to spread scraping bots to prevend blocking

--- Readme.md
+++ Readme.md
@@ -10,6 +10,10 @@ Go to [Pushover](https://pushover.net/), create an account, an application and d
 ## Install
 After you've set the Enviroment Variables, install docker and docker-compose and docker-compose up -d should do the rest :)
 
+## Install additional selenium Servers
+Run the following Command \
+`` apt-get update && apt-get upgrade -y && apt-get install docker.io -y && docker run -d -p 4444:4444 selenium/standalone-chrome:4.0.0``
+
 ## Todos
 - Twitter Integration
 - Check which dates are available and include them in the notification
--- main.py
+++ main.py
@@ -8,10 +8,11 @@ request = requests.post("https://api.pushover.net/1/messages.json", pushData)
 
 # Locations Array is defined in the following structure: Array[Array[Name, Vaccination Center Page, REST Api for appointment check]]
 locations = [["Hamburg Messehallen","https://353-iz.impfterminservice.de/impftermine/service?plz=20357", "https://353-iz.impfterminservice.de/rest/suche/termincheck?plz=20357&leistungsmerkmale=L920,L921,L922,L923"],["Tübingen Impfzentrum","https://003-iz.impfterminservice.de/impftermine/service?plz=72072", "https://003-iz.impfterminservice.de/rest/suche/termincheck?plz=72072&leistungsmerkmale=L920,L921,L922,L923"]]
+servers = ["http://selenium:4444/wd/hub","http://10.0.0.3:4444/wd/hub","http://10.0.0.4:4444/wd/hub","http://10.0.0.5:4444/wd/hub","http://10.0.0.6:4444/wd/hub"]
 
-def scrapePage(locationData):
+def scrapePage(locationData, remote):
     # Click through the impftermineservice page to act like a human lol
-    driver = webdriver.Remote("http://selenium:4444/wd/hub", DesiredCapabilities.CHROME)
+    driver = webdriver.Remote(remote, DesiredCapabilities.CHROME)
     driver.get(locationData[1])
     time.sleep(1)
     driver.find_element_by_css_selector("body > app-root > div > app-page-its-login > div > div > div:nth-child(2) > app-its-login-user > div > div > app-corona-vaccination > div:nth-child(2) > div > div > label:nth-child(2) > span > small").click()
@@ -28,25 +29,26 @@ def scrapePage(locationData):
         pushData = {"token": os.environ['token'], "user": os.environ['user'], "message": "Der Bot wurde in "+locationData[0]+" gesperrt :(", "priority": "1"}
         requests.post("https://api.pushover.net/1/messages.json", pushData)
         time.sleep(600)
-
-    # Decode json data
-    data = json.loads(jsonData)
-
-    # Map json data to variables
-    available = data["termineVorhanden"]
-    types = data["vorhandeneLeistungsmerkmale"]
-
-    # If an appointment is available send an notification to your device, if not send a message without notification which is shown in the Pushover App
-    if(available):
-        pushData = {"token":os.environ['token'],"user":os.environ['user'],"title":"Es gibt Impftermine in "+locationData[0]+" !!!", "message": json.dumps(types), "priority":"1"}
-        requests.post("https://api.pushover.net/1/messages.json", pushData)
     else:
-        pushData = {"token": os.environ['token'], "user": os.environ['user'], "message": "Impftermine in " + locationData[0] + " geprüft, gibt aber keine :(", "priority": "-2"}
-        requests.post("https://api.pushover.net/1/messages.json", pushData)
+        # Decode json data
+        data = json.loads(jsonData)
+
+        # Map json data to variables
+        available = data["termineVorhanden"]
+        types = data["vorhandeneLeistungsmerkmale"]
+
+        # If an appointment is available send an notification to your device, if not send a message without notification which is shown in the Pushover App
+        if(available):
+            pushData = {"token":os.environ['token'],"user":os.environ['user'],"title":"Es gibt Impftermine in "+locationData[0]+" !!!", "message": json.dumps(types), "priority":"1"}
+            requests.post("https://api.pushover.net/1/messages.json", pushData)
+        else:
+            pushData = {"token": os.environ['token'], "user": os.environ['user'], "message": "Impftermine in " + locationData[0] + " mit Server "+remote+" geprüft, gibt aber keine :(", "priority": "-2"}
+            requests.post("https://api.pushover.net/1/messages.json", pushData)
 
 while(True):
     # Wait until the selenium container initialized
     time.sleep(10)
-    for location in locations:
-        scrapePage(location)
-    time.sleep(200)
+    for server in servers:
+        for location in locations:
+            scrapePage(location, server)
+            time.sleep(40)
